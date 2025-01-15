e5ec3da481f60fa784f46d62208538a0f1781cf5
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-15 20:04:18 -0300

    feat: cria a navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import './Navbar.css';
+import logo from '../../assets/logo-technologies-04.svg';
+
+const Navbar: React.FC = () => {
+    return (
+        <header className="navbar">
+            <div className="navbar-logo">
+                <img src={logo} alt="Technologies Mercury Logo" />
+            </div>
+            <nav className="navbar-nav">
+                <ul className="navbar-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#orders">Orders</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
