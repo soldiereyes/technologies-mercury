d31294a9986c81abee8db76d85c41736691f4e96
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-15 20:04:36 -0300

    feat: implementação da navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/navbar/Navbar.tsx";
 
-function App() {
+const App: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <main>
+                <section id="home">Welcome to Technologies Mercury</section>
+                {/* Outras seções */}
+            </main>
+        </div>
+    );
+};
 
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default App
+export default App;
