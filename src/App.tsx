c62e982f7d0f729cba5d6a0020752d314aa527e5
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-16 17:54:47 -0300

    feat: cria footer, ainda não finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Navbar from "./components/navbar/Navbar.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 const App: React.FC = () => {
     return (
         <div>
             <Navbar />
             <main>
-                <section id="home">Welcome to Technologies Mercury</section>
-                {/* Outras seções */}
+                <section id="home" style={{ padding: '2rem', textAlign: 'center' }}>
+                    Welcome to Technologies Mercury
+                </section>
             </main>
+            <Footer />
         </div>
     );
 };
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,38 @@
+import './Footer.css';
+import logo from '../../assets/logo-technologies-04.svg';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-left">
+                    <div className="footer-logo">
+                        <img src={logo} alt="Technologies Mercury Logo" />
+                        <h3>Technologies Mercury</h3>
+                    </div>
+                    <p>Call us if you need some help</p>
+                    <p className="footer-phone">+000 00 000 0000</p>
+                </div>
+                <div className="footer-links">
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li>Services
+                            <ul className="sub-links">
+                                <li><a href="#crude-oil">Crude oil and oil products trading</a></li>
+                                <li><a href="#urea">Urea</a></li>
+                                <li><a href="#oil-drilling">Oil Drilling</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#orders">Orders</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>Copyright © 2025 Technologies Mercury - All rights reserved</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
