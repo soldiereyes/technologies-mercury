a00d5ea31b63cc867c650a87bdb401ceddaf7cb8
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-21 17:52:25 -0300

    feat: inclui Banner inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar.tsx";
 import Footer from "./components/footer/Footer.tsx";
+import Banner from "./components/banner-into/Banner.tsx";
 
 const App: React.FC = () => {
     return (
         <div>
             <Navbar />
+            <Banner />
             <main>
                 <section id="home" style={{ padding: '2rem', textAlign: 'center' }}>
                     Welcome to Technologies Mercury
