3e6a1e84db1c197c4abb866e58af4699255c494a
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-21 17:55:55 -0300

    feat: configuração do bootstrab na navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,45 @@
 import './Navbar.css';
-import logo from '../../assets/images/logo-technologies-04.svg';
+import logo from '../../assets/images/logo-technologies-04.svg'
 
 const Navbar: React.FC = () => {
     return (
-        <header className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="Technologies Mercury Logo" />
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <a className="navbar-brand" href="#home">
+                    <img src={logo} alt="Logo" height="60" />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#about">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#services">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#orders">Orders</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <nav className="navbar-nav">
-                <ul className="navbar-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About Us</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#orders">Orders</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
+        </nav>
     );
 };
 
