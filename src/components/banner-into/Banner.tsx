ff076d064bf3c952fc03cd8eccdaf5611c3cba2a
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-24 12:24:16 -0300

    feat: ajustes no banner inicial

--- src/components/banner-into/Banner.tsx
+++ src/components/banner-into/Banner.tsx
@@ -1,14 +1,20 @@
+
 import './Banner.css';
 
 const Banner: React.FC = () => {
     return (
-        <section className="banner d-flex flex-column justify-content-center align-items-center">
-            <div className="container text-center text-white">
-                <h1 className="display-4 fw-bold">Proin vel ligula vel libero faucibus consequat</h1>
-                <p className="lead">
-                    Nullam dignissim, orci non fermentum malesuada, eros ipsum gravida velit.
-                </p>
-                <button className="btn btn-primary btn-lg mt-3">Saiba Mais</button>
+        <section className="banner d-flex align-items-center">
+            <div className="banner-overlay"></div>
+            <div className="container">
+                <div className="banner-content text-white">
+                    <h1 className="banner-title">
+                        Proin vel ligula vel libero <br /> faucibus consequat.
+                    </h1>
+                    <p className="banner-subtitle">
+                        Nullam dignissim, orci non fermentum malesuada, eros ipsum gravida velit, id fermentum lorem nisi non enim.
+                    </p>
+                    <button className="btn btn-primary btn-lg mt-3">Saiba Mais</button>
+                </div>
             </div>
         </section>
     );
