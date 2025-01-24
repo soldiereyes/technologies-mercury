ce807e4f9d6ba9f0033bb2bc4255f0d9379a198c
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-24 12:24:41 -0300

    feat: remoção de sessão teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import Navbar from "./components/navbar/Navbar.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Banner from "./components/banner-into/Banner.tsx";
+import Statistics from "./components/banner-statistics/Statistics.tsx";
 
 const App: React.FC = () => {
     return (
         <div>
             <Navbar />
             <Banner />
-            <main>
-                <section id="home" style={{ padding: '2rem', textAlign: 'center' }}>
-                    Welcome to Technologies Mercury
-                </section>
-            </main>
+            <Statistics />
             <Footer />
         </div>
     );
