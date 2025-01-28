a6eb18fc64514dbc110a8f8ef847b111d60b3693
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-28 17:15:36 -0300

    feat: implementa as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import Navbar from "./components/navbar/Navbar.tsx";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Footer from "./components/footer/Footer.tsx";
-import Banner from "./components/banner-into/Banner.tsx";
-import Statistics from "./components/banner-statistics/Statistics.tsx";
-import {CardSection} from "./components/card-section/CardSection.tsx";
-import HeroSection from "./components/hero-section/HeroSection.tsx";
+import Home from "./pages/home/Home.tsx";
+import About from "./pages/about/About.tsx";
+import Services from "./pages/services/Services.tsx";
+import Orders from "./pages/orders/Orders.tsx";
+import Contact from "./pages/contact/Contact.tsx";
+import ContactSection from "./components/contact-section/ContactSection.tsx";
 
 const App: React.FC = () => {
     return (
         <div>
-            <Navbar />
-            <Banner />
-            <Statistics />
-            <CardSection/>
-            <HeroSection/>
-            <Footer />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <ContactSection/>
+                <Footer />
+            </Router>
+
         </div>
     );
 };
