1708a44a2bf024f2a22933238958fda45ab87ffb
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-30 17:16:44 -0300

    feat: implementa os componentes da página

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,15 @@
 import AboutBanner from "../../components/about-banner/AboutBanner.tsx";
+import FirstAboutSection from "../../components/first-about-section/FirstAboutSection.tsx";
+import SecondSectionAbout from "../../components/second-about-section/SecondAboutSection.tsx";
+import LocationMap from "../../components/Location/LocationMap.tsx";
 
 const About: React.FC = () => {
     return (
         <div>
-            <AboutBanner />
-
+            <AboutBanner/>
+            <FirstAboutSection/>
+            <SecondSectionAbout/>
+            <LocationMap/>
         </div>
     );
 };
