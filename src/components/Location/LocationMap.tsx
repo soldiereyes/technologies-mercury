b47e53ad8f820512437ab72fc77fd2e729c02212
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-01-30 17:18:05 -0300

    feat: constroe o componente map

---
+++ src/components/Location/LocationMap.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./LocationMap.css";
+
+const center = {
+    lat: 50.0755,
+    lng: 14.4378,
+};
+
+const LocationMap: React.FC = () => {
+    return (
+        <section className="location-section">
+            <MapContainer center={center} zoom={13} scrollWheelZoom={false} className="map-container">
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={center}>
+                    <Popup>Localização da Empresa</Popup>
+                </Marker>
+            </MapContainer>
+        </section>
+    );
+};
+
+export default LocationMap;
