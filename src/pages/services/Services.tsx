96f2968ca569bdc1dcb4ed1ef1434df4d31ddabb
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-02-07 15:38:46 -0300

    feat: refina banner de estatísticas.

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,9 +1,22 @@
+import CrudeOilBanner from "../../components/services-pages/crud-oil/CrudOilBanner.tsx";
+import SecondCrudOil from "../../components/services-pages/crud-oil/SecondCrudOil.tsx";
+import PetroleumProductsSection from "../../components/services-pages/crud-oil/PetroleumProductsSection.tsx";
+import OilDrillingBanner from "../../components/services-pages/oil-drilling/OilDrillingBanner.tsx";
+import OilDrillingSection from "../../components/services-pages/oil-drilling/OilDrillingSection.tsx";
+import UreaBanner from "../../components/services-pages/urea/UreaBanner.tsx";
+import UreaSection from "../../components/services-pages/urea/UreaSection.tsx";
+
 const Services: React.FC = () => {
     return (
-        <div className="container mt-5">
-            <h1>Our Services</h1>
-            <p>Detalhes sobre os serviços oferecidos...</p>
-        </div>
+        <>
+            {/*<CrudeOilBanner/>*/}
+            {/*<SecondCrudOil/>*/}
+            {/*<PetroleumProductsSection/>*/}
+            {/*<OilDrillingBanner/>*/}
+            {/*<OilDrillingSection/>*/}
+            <UreaBanner/>
+            <UreaSection/>
+        </>
     );
 };
 
