df2cd4534b3f72393ca2a927940aa98c3e076538
Guilherme Gonzaga Soares <ggonzaga@destaxa.com.br>
2025-02-07 15:01:21 -0300

    feat: refina banner inicial

--- src/components/banner-into/Banner.tsx
+++ src/components/banner-into/Banner.tsx
@@ -1,20 +1,23 @@
-
 import './Banner.css';
 
 const Banner: React.FC = () => {
     return (
-        <section className="banner d-flex align-items-center">
-            <div className="banner-overlay"></div>
+        <section className="banner">
+            <div className="banner-into-overlay">
             <div className="container">
-                <div className="banner-content text-white">
-                    <h1 className="banner-title">
+                <div className="banner-into-content">
+                    <span className="banner-tagline">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </span>
+                    <h1 className="banner-into-title">
                         Proin vel ligula vel libero <br /> faucibus consequat.
                     </h1>
                     <p className="banner-subtitle">
                         Nullam dignissim, orci non fermentum malesuada, eros ipsum gravida velit, id fermentum lorem nisi non enim.
                     </p>
-                    <button className="btn btn-primary btn-lg mt-3">Saiba Mais</button>
+                    <button className="cta-into-button">Saiba Mais</button>
                 </div>
+            </div>
             </div>
         </section>
     );
